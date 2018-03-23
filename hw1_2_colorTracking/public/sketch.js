45bc47484685366f816f5938c106c10a5b0c7728
pondjames007 <jameshuang@nyu.edu>
2018-03-23 19:42:30 -0400

    hw 1-2

---
+++ hw1_2_colorTracking/public/sketch.js
@@ -0,0 +1,25 @@
+// Draw with the mouse
+
+
+  window.onload = function() {
+    var video = document.getElementById('video');
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var tracker = new tracking.ColorTracker();
+    tracking.track('#video', tracker, { camera: true });
+    tracker.on('track', function(event) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      event.data.forEach(function(rect) {
+        if (rect.color === 'custom') {
+          rect.color = tracker.customColor;
+        }
+        context.strokeStyle = rect.color;
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        context.font = '11px Helvetica';
+        context.fillStyle = "#fff";
+        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      });
+    });
+   initGUIControllers(tracker);
+  };
